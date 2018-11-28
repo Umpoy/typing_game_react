d1cd40b59e57f605969230993a16d080fe79e39e
Umpoy <iumpoy@yahoo.com>
2018-11-28 07:05:25 -0800

    fixed bug that wouldnt let me change state

--- src/components/app.js
+++ src/components/app.js
@@ -13,39 +13,42 @@ class App extends Component {
         }
 
         this.handleKeyPress = this.handleKeyPress.bind(this);
+        this.map = this.state.words.map((word) =>
+            <div>{word}</div>
+        )
 
     }
     handleKeyPress(event) {
-        //console.log(event.key);
+        console.log(event.key)
         if (this.state.words[0] == '') {
+            if (event.key !== ' ') {
+                console.log('need to press space bar');
+                return
+            }
             let hold = this.state.words.slice(1);
             this.setState({
                 words: hold
             })
         }
-        //console.log(this.state.words[0][0])
         if (this.state.words[0][0] == event.key) {
+
             let holdArray = this.state.words
             console.log('holdArray: ', holdArray)
             let hold = holdArray[0].replace(this.state.words[0][0], '');
             holdArray[0] = hold
-            console.log('holdArray2: ', holdArray)
-            console.log('hold: ', hold)
-            // this.state.words[0] = hold
+            console.log(holdArray)
             this.setState({
-                words: hold
+                words: holdArray
             })
-            console.log(this.state)
+            console.log(this.state.words)
         }
     }
 
     render() {
-        const map = this.state.words.map((words) =>
-            <div>{words}</div>
-        )
+
         return (
             <div>
-                <div >{map}</div>
+                <div >{this.map}</div>
                 <div>
                     <input type="text" id="one" onKeyPress={this.handleKeyPress} />
                 </div>
