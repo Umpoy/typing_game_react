a579395db17cafea87912fff706dce8d0b90fbf7
Umpoy <iumpoy@yahoo.com>
2018-11-27 19:25:45 -0800

    able to change state on keypress

--- src/components/app.js
+++ src/components/app.js
@@ -11,23 +11,35 @@ class App extends Component {
                 "tree"
             ]
         }
+
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+
     }
-    handleKeyPress = (event) => {
-        console.log(event.key);
+    handleKeyPress(event) {
+        //console.log(event.key);
         if (this.state.words[0] == '') {
             let hold = this.state.words.slice(1);
-            this.state.words = hold;
+            this.setState({
+                words: hold
+            })
         }
         //console.log(this.state.words[0][0])
         if (this.state.words[0][0] == event.key) {
-            let hold = this.state.words[0].replace(this.state.words[0][0], '')
-            this.state.words[0] = hold
+            let holdArray = this.state.words
+            console.log('holdArray: ', holdArray)
+            let hold = holdArray[0].replace(this.state.words[0][0], '');
+            holdArray[0] = hold
+            console.log('holdArray2: ', holdArray)
+            console.log('hold: ', hold)
+            // this.state.words[0] = hold
+            this.setState({
+                words: hold
+            })
             console.log(this.state)
         }
     }
 
     render() {
-        console.log(this.state)
         const map = this.state.words.map((words) =>
             <div>{words}</div>
         )
